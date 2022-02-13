7b1593f18446ffcfa0ef4bb0723b5ebd6f46a847
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-13 19:02:37 +0300

    Added last sidebar functions

--- src/context.js
+++ src/context.js
@@ -3,8 +3,23 @@ import React, { useState, useContext } from 'react'
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
     return (
-        <AppContext.Provider value="hello"> {children} </AppContext.Provider>
+        <AppContext.Provider value={{isModalOpen, isSidebarOpen, openModal, openSidebar, closeModal, closeSidebar}}> {children} </AppContext.Provider>
     )
 }
 
